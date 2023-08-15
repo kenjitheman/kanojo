3eb01892d9b0ddd90a9a65b27d6663b867b49d37
kenjitheman <kenjitheman@proton.me>
2023-08-15 20:55:41 +0300

    feat: gdrive saving logic

--- gdrive/gdrive.go
+++ gdrive/gdrive.go
@@ -1,9 +1,67 @@
 package gdrive
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
+	drive "google.golang.org/api/drive/v3"
 )
 
-func GDrive() {
+func ServiceAccount(credentialFile string) *http.Client {
+	b, err := os.ReadFile(credentialFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := struct {
+		Email      string `json:"client_email"`
+		PrivateKey string `json:"private_key"`
+	}{}
+	json.Unmarshal(b, &c)
+	config := &jwt.Config{
+		Email:      c.Email,
+		PrivateKey: []byte(c.PrivateKey),
+		Scopes: []string{
+			drive.DriveScope,
+		},
+		TokenURL: google.JWTTokenURL,
+	}
+	client := config.Client(oauth2.NoContext)
+	return client
+}
 
+func Gdrive(fileName string) {
+	filename := fileName
+	baseMimeType := "text/plain"
+	client := ServiceAccount("../credentials.json") // set the json file of service account
+
+	srv, err := drive.New(client)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fileInf, err := file.Stat()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	f := &drive.File{Name: filename}
+	res, err := srv.Files.
+		Create(f).
+		ResumableMedia(context.Background(), file, fileInf.Size(), baseMimeType).
+		ProgressUpdater(func(now, size int64) { fmt.Printf("%d, %d\r", now, size) }).
+		Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s\n", res.Id)
 }
--- openai/openai.go
+++ openai/openai.go
@@ -10,9 +10,11 @@ import (
 
 	"github.com/joho/godotenv"
 	openai "github.com/sashabaranov/go-openai"
+
+	"main.go/core"
 )
 
-func GenerateImage() {
+func GenerateImage(prompt string) {
 	err := godotenv.Load("../.env")
 	if err != nil {
 		fmt.Println("[ERROR] error loading .env file", err)
@@ -20,59 +22,59 @@ func GenerateImage() {
 	c := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 	ctx := context.Background()
 
-	// Sample image by link
 	reqUrl := openai.ImageRequest{
-		Prompt:         "Parrot on a skateboard performs a trick, cartoon style, natural light, high detail",
-		Size:           openai.CreateImageSize256x256,
+		Prompt:         prompt,
+		Size:           openai.CreateImageSize1024x1024,
 		ResponseFormat: openai.CreateImageResponseFormatURL,
 		N:              1,
 	}
 
 	respUrl, err := c.CreateImage(ctx, reqUrl)
 	if err != nil {
-		fmt.Printf("Image creation error: %v\n", err)
+		fmt.Printf("[ERROR] image creation error: %v\n", err)
 		return
 	}
 	fmt.Println(respUrl.Data[0].URL)
 
-	// Example image as base64
 	reqBase64 := openai.ImageRequest{
-		Prompt:         "Portrait of a humanoid parrot in a classic costume, high detail, realistic light, unreal engine",
-		Size:           openai.CreateImageSize256x256,
+		Prompt:         prompt,
+		Size:           openai.CreateImageSize1024x1024,
 		ResponseFormat: openai.CreateImageResponseFormatB64JSON,
 		N:              1,
 	}
 
 	respBase64, err := c.CreateImage(ctx, reqBase64)
 	if err != nil {
-		fmt.Printf("Image creation error: %v\n", err)
+		fmt.Printf("[ERROR] image creation error: %v\n", err)
 		return
 	}
 
 	imgBytes, err := base64.StdEncoding.DecodeString(respBase64.Data[0].B64JSON)
 	if err != nil {
-		fmt.Printf("Base64 decode error: %v\n", err)
+		fmt.Printf("[ERROR] base64 decode error: %v\n", err)
 		return
 	}
 
 	r := bytes.NewReader(imgBytes)
 	imgData, err := png.Decode(r)
 	if err != nil {
-		fmt.Printf("PNG decode error: %v\n", err)
+		fmt.Printf("[ERROR] PNG decode error: %v\n", err)
 		return
 	}
 
-	file, err := os.Create("example.png")
+	fileName := core.GenerateRandomFileName() + ".png"
+
+	file, err := os.Create(fileName)
 	if err != nil {
-		fmt.Printf("File creation error: %v\n", err)
+		fmt.Printf("[ERROR] file creation error: %v\n", err)
 		return
 	}
 	defer file.Close()
 
 	if err := png.Encode(file, imgData); err != nil {
-		fmt.Printf("PNG encode error: %v\n", err)
+		fmt.Printf("[ERROR] PNG encode error: %v\n", err)
 		return
 	}
 
-	fmt.Println("The image was saved as example.png")
+	fmt.Printf("[SUCCESS] the image was saved as %s", fileName)
 }
